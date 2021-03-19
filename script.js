e6893bbd6b1241051e3328be0c8d2859b32d523b
sa_rakib <rakibsamsuralam@gmail.com>
2021-03-19 15:28:05 +0600

    all complite bank website

--- script.js
+++ script.js
@@ -11,28 +11,36 @@ loginBtn.addEventListener('click', function(){
 //deposit button event handler //
 const depositBtn = document.getElementById("addDeposit");
 depositBtn.addEventListener('click', function(){
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositNumber = parseFloat(depositAmount);
+    const depositNumber = getInputNumber("depositAmount");
 
-
-    const currentDeposit = document.getElementById("currentDeposit").innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
-
-
-    document.getElementById("currentDeposit").innerText = totalDeposit;
-
-//----------- currentBalance Number section ------------//
-    
+updateSpanText("currentDeposit", depositNumber);   
 updateSpanText("currentBalance", depositNumber);
 
-    document.getElementById("depositAmount").value = "";
+document.getElementById("depositAmount").value = "";
 })
 
+// withdraw button event handler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener('click', function(){
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber);
+
+    document.getElementById("withdrawAmount").value = "";
+})
+
+function getInputNumber(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+
+
 function updateSpanText(id, depositNumber){
-    const currentBalance = document.getElementById(id).innerText;
-    const currentBalanceNumber = parseFloat(currentBalance);
-    const totalBalance = depositNumber + currentBalanceNumber;
-    document.getElementById(id).innerText = totalBalance;
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
 }
 
